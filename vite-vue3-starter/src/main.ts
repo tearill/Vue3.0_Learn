d617cadc816a57049bbb59c80496f80d05d38c7c
caigang <caigang@baidu.com>
2021-04-22 14:01:05 +0800

    build:add router
    Change-Id: I6a4c548ea306fe821efd0f29f29a2c02bd839f00

--- vite-vue3-starter/src/main.ts
+++ vite-vue3-starter/src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// 挂载路由
+import router from './router/index';
+
+createApp(App).use(router).mount('#app');
