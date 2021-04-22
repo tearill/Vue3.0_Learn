d6f6ce91fae50e0c252a906fa3db381e48060168
caigang <caigang@baidu.com>
2021-04-22 14:34:13 +0800

    build:add vuex
    Change-Id: I2056457efc894f73715b954519526d4109723914

--- vite-vue3-starter/src/main.ts
+++ vite-vue3-starter/src/main.ts
@@ -4,4 +4,10 @@ import App from './App.vue';
 // 挂载路由
 import router from './router/index';
 
-createApp(App).use(router).mount('#app');
+// 挂载 store
+import store from './store/index'
+
+createApp(App)
+    .use(router)
+    .use(store)
+    .mount('#app');
