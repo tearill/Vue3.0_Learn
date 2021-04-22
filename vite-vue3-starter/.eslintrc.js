3e92c72169bb5e1da12138cd3498c60088431298
caigang <caigang@baidu.com>
2021-04-22 16:00:03 +0800

    chore:change eslint style to airbnb
    Change-Id: I8e764a8a79c7a50d92a67b4e8285c08eb7494cdf

--- vite-vue3-starter/.eslintrc.js
+++ vite-vue3-starter/.eslintrc.js
@@ -1,52 +1,23 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true,
-		'node': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:vue/essential',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended'
-	],
-	'parserOptions': {
-		'ecmaVersion': 12,
-		'parser': '@typescript-eslint/parser',
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'vue',
-		'@typescript-eslint'
-	],
-	'rules': {
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		],
-		'@typescript-eslint/camelcase': [
-			'error',
-			'off'
-		],
-        '@typescript-eslint/no-use-before-define': [
-			'error',
-			'off'
-		],
-        '@typescript-eslint/array-type': [
-			'error',
-			'off'
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+	'airbnb-base',
+	'plugin:prettier/recommended' // 添加 prettier 插件
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+  },
 };
