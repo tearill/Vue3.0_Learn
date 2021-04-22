348c6aad328b74826a1ebe01a4d37b10924810c6
caigang <caigang@baidu.com>
2021-04-22 14:36:31 +0800

    build:add element-plus
    Change-Id: I60d016f89c90703d0aeae1c4c411a6c772629321

--- vite-vue3-starter/src/main.ts
+++ vite-vue3-starter/src/main.ts
@@ -7,7 +7,12 @@ import router from './router/index';
 // 挂载 store
 import store from './store/index';
 
+// 挂载element-ui
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
 createApp(App)
     .use(router)
     .use(store)
+    .use(ElementPlus)
     .mount('#app');
