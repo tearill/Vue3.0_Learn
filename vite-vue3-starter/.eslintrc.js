6ba4c5390f88cb6ab591d55bca184daf434cffed
caigang <caigang@baidu.com>
2021-04-22 15:39:10 +0800

    style:add eslint rules
    Change-Id: I56231068fe66fbaeca8e267604e1dd0a6e5772d3

---
+++ vite-vue3-starter/.eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:vue/essential',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parserOptions': {
+		'ecmaVersion': 12,
+		'parser': '@typescript-eslint/parser',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'vue',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'@typescript-eslint/camelcase': [
+			'error',
+			'off'
+		],
+        '@typescript-eslint/no-use-before-define': [
+			'error',
+			'off'
+		],
+        '@typescript-eslint/array-type': [
+			'error',
+			'off'
+		],
+        'init-declarations': 'error',
+        'no-var': 'error',
+        'no-console': 'error'
+	}
+};
