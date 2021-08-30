1f7f7e94fbeb9d7c4cc555df46fd3d9186d51305
caigang <caigang@baidu.com>
2021-08-30 22:56:04 +0800

    chore: eslint
    Change-Id: Ia9a15d61fdc5182856fee1a830adff8fbc531b34

--- learn-vue3/.eslintrc.js
+++ learn-vue3/.eslintrc.js
@@ -1,36 +1,38 @@
 module.exports = {
     root: true,
     env: {
-        browser: true,
-        node: true,
-        es6: true
+        node: true
     },
-    globals: {
-        ga: true,
-        chrome: true
-    },
-    plugins: ["vue"],
-    parser: "vue-eslint-parser",
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/typescript/recommended'
+    ],
     parserOptions: {
-        ecmaVersion: 6,
-        parser: "babel-eslint",
-        ecmaFeatures: {
-            jsx: true
-        },
-        sourceType: "module"
+        ecmaVersion: 2020
     },
     rules: {
-        "no-restricted-globals": ["error", "event", "fdescribe"],
-        // allow async-await
-        "generator-star-spacing": "off",
-        //语句强制分号结尾
-        semi: [2, "always"],
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        semi: ['error', 'always'],
         // 缩进
         indent: ["error", 4, { "SwitchCase": 1 }],
         // 方法名与括号的空格
         "space-before-function-paren": 0,
         "brace-style": [2, "stroustrup"],
         // 符号在一行的开头
-        "operator-linebreak": ["error", "before"]
-    }
-}
+        "operator-linebreak": ["error", "before"],
+        "no-restricted-globals": ["error", "event", "fdescribe"],
+        // allow async-await
+        "generator-star-spacing": "off"
+    },
+    overrides: [{
+        files: [
+            '**/__tests__/*.{j,t}s?(x)',
+            '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        ],
+        env: {
+            jest: true
+        }
+    }]
+};
