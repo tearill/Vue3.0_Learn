c7605b457d38a6dfb756feebc439f4b4d38ae807
caigang <caigang@baidu.com>
2021-10-29 14:55:11 +0800

    feat: style:v-bind

--- vite-project-js/src/pages/home.vue
+++ vite-project-js/src/pages/home.vue
@@ -1,10 +1,13 @@
 <template>
     <h1>Home Page</h1>
     <TodoList/>
+
+    <Add/>
 </template>
 
 <script setup>
 import TodoList from '../components/TodoList.vue';
+import Add from '../components/Add.vue';
 </script>
 
 <style>
